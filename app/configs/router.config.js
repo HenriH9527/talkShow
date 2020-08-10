1f672bbc78d830cac8145137b49a54efdec93372
VincentHyf <1004779785.com>
2020-08-10 20:54:18 +0800

    ADD(router.config.js): Routing controler

---
+++ app/configs/router.config.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router, Route, IndexRoute, hashHistory/* , Redirect */ } from 'react-router'
+
+import * as base from '@pages/base'
+
+export default () => {
+    <Router history={hashHistory}>
+        <Route path="/" component={base.app}>
+    
+        </Route>
+    </Router>
+}
